c83dbf20fa0fc44c332460cadb890857d3c850bb
Kelly Adams <kelly@intellitect.com>
2024-11-16 13:57:26 -0800

    Fix lint errors

--- SyncUp.Web/src/views/Post.vue
+++ SyncUp.Web/src/views/Post.vue
@@ -28,28 +28,41 @@
       variant="flat"
       @click="showComments = !showComments"
     >
-      <v-icon :icon="showComments ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'"></v-icon
+      <v-icon
+        :icon="
+          showComments ? 'fa-solid fa-chevron-down' : 'fa-solid fa-chevron-up'
+        "
+      ></v-icon
     ></v-btn>
     <v-btn class="mt-3" rounded="xl" @click="showAddComment = !showAddComment">
       <v-icon icon="fa-solid fa-plus"></v-icon>
       <span>Add a comment</span>
     </v-btn>
-    <div class="mt-3 ms-8" v-if="showAddComment">
+    <div v-if="showAddComment" class="mt-3 ms-8">
       <v-card>
         <v-card-text>
           <c-input :model="comment" for="body"></c-input>
           <div class="text-right">
-            <v-btn class="me-2" variant="flat" @click="showAddComment = !showAddComment" >cancel</v-btn>
-            <v-btn color="primary" variant="flat" @click="saveComment">save</v-btn>
+            <v-btn
+              class="me-2"
+              variant="flat"
+              @click="showAddComment = !showAddComment"
+              >cancel</v-btn
+            >
+            <v-btn color="primary" variant="flat" @click="saveComment"
+              >save</v-btn
+            >
           </div>
         </v-card-text>
       </v-card>
     </div>
-    <template v-for="comment in comments.$items" :key="comment.$stableId">
+    <template v-for="c in comments.$items" :key="c.$stableId">
       <v-card v-if="showComments" class="mt-3 ms-8">
-        <v-card-title>{{ comment.createdBy?.userName ?? "unknown user" }}</v-card-title>
+        <v-card-title>{{
+          c.createdBy?.userName ?? "unknown user"
+        }}</v-card-title>
         <v-card-text>
-          {{ comment.body }}
+          {{ c.body }}
         </v-card-text>
       </v-card>
     </template>
@@ -77,14 +90,14 @@ const comments = new CommentListViewModel();
 const dataSource = new Comment.DataSources.CommentsForPost();
 dataSource.postId = props.postId;
 comments.$dataSource = dataSource;
-comments.$load(); 
+comments.$load();
 
-const comment = new CommentViewModel(); 
+const comment = new CommentViewModel();
 async function saveComment() {
   comment.postId = post.postId;
   await comment.$save();
   showAddComment = ref(false);
-  comments.$load(); 
+  comments.$load();
 }
 
 let showAddComment = ref(false);
