289a9aa0d585f6d62b2ab9d0534df0ff86425afb
Kelly Adams <kelly@intellitect.com>
2024-11-15 20:39:28 -0800

    Create new card to add new group

--- SyncUp.Web/src/views/Home.vue
+++ SyncUp.Web/src/views/Home.vue
@@ -10,24 +10,33 @@
       :is-member="isMember"
       @toggle-membership="toggleMembership"
     />
-    <v-row class="mt-1">
-      <v-col
-        v-for="group in groups.$items"
-        :key="group.groupId!"
-        cols="12"
-        md="6"
-        lg="4"
-      >
-        <GroupCard :group="group" />
+
+    <v-row class="ma-1">
+      <v-col cols="12" md="6" lg="8">
+        <AddGroupCard :group="newGroup" />
+      </v-col>
+      <v-col cols="12" md="6" lg="8">
+        <v-row>
+          <v-col
+            v-for="group in groups.$items"
+            :key="group.groupId!"
+            cols="12"
+            md="6"
+            lg="4"
+          >
+            <GroupCard :group="group" />
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { GroupListViewModel, TenantListViewModel } from "@/viewmodels.g";
+import { GroupListViewModel, TenantListViewModel, GroupViewModel } from "@/viewmodels.g";
 
 useTitle("Home");
+const newGroup = new GroupViewModel();
 const groups = new GroupListViewModel();
 groups.$useAutoLoad();
 groups.$load();
