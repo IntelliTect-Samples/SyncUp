d8a1cfa2069b5873ac51334b8b0127b61616b19d
Kelly Adams <kelly@intellitect.com>
2024-11-16 11:46:38 -0800

    Show validation stuff

--- SyncUp.Web/src/components/AddGroupButton.vue
+++ SyncUp.Web/src/components/AddGroupButton.vue
@@ -10,31 +10,41 @@
   </v-btn>
 
   <v-dialog v-model="show" max-width="500">
-    <v-card title="Add A New Group">
-      <v-card-text>
-        <c-input :model="group" for="name" />
-        <c-input :model="group" for="description" />
-        <c-input :model="group" for="imageUrl" />
-      </v-card-text>
-      <v-divider class="mt-2"></v-divider>
+    <v-card>
+      <v-sheet color="primary">
+        <v-card-title> Add A New Group</v-card-title>
+      </v-sheet>
+      <c-loader-status
+        :loaders="{
+          '': [group.$save],
+        }"
+      >
+        <v-card-text>
+          <c-input :model="group" for="name" />
+          <c-input :model="group" for="description" />
+          <c-input :model="group" for="imageUrl" />
+        </v-card-text>
+        <v-divider class="mt-2"></v-divider>
 
-      <v-card-actions class="my-2 d-flex justify-end">
-        <v-btn
-          class="text-none"
-          rounded="xl"
-          text="Cancel"
-          @click="show = false"
-        ></v-btn>
+        <v-card-actions class="my-2 d-flex justify-end">
+          <v-btn
+            class="text-none"
+            rounded="xl"
+            text="Cancel"
+            @click="show = false"
+          ></v-btn>
 
-        <v-btn
-          class="text-none"
-          color="primary"
-          rounded="xl"
-          text="Save"
-          variant="flat"
-          @click="saveGroup"
-        ></v-btn>
-      </v-card-actions>
+          <v-btn
+            class="text-none"
+            color="primary"
+            rounded="xl"
+            text="Save"
+            variant="flat"
+            :loading="group.$save.isLoading"
+            @click="saveGroup"
+          ></v-btn>
+        </v-card-actions>
+      </c-loader-status>
     </v-card>
   </v-dialog>
 </template>
