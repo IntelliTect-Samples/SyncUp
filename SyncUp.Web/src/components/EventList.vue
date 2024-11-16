28da67d262e2825f16aa10ee40417fb99aa4cfd7
MikaellaCroskrey <Mikaella.Croskrey@Intellitect.com>
2024-11-16 13:50:22 -0800

    Add loader and fix spacing

--- SyncUp.Web/src/components/EventList.vue
+++ SyncUp.Web/src/components/EventList.vue
@@ -1,40 +1,58 @@
 <template>
-  <v-row dense>
-    <v-col>
-      <v-text-field
-        v-model="events.$params.search"
-        label="Search Events"
-        prepend-inner-icon="fa fa-search"
-        hide-details
-        clearable
-      />
-    </v-col>
-    <v-col cols="auto" align="right">
-      <c-input :model="eventsDataSource" for="showPastEvents" />
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col
-      v-for="event in events.$items"
-      :key="event.eventId!"
-      cols="12"
-      md="6"
-      lg="4"
-    >
-      <EventCard :event="event" />
-    </v-col>
-  </v-row>
+  <c-loader-status :loaders="[events.$load]">
+    <v-row dense>
+      <v-col>
+        <v-text-field
+          v-model="events.$params.search"
+          label="Search Events"
+          prepend-inner-icon="fa fa-search"
+          hide-details
+          clearable
+        />
+      </v-col>
+      <v-col cols="auto" align="right">
+        <c-input
+          :model="eventsDataSource"
+          for="showPastEvents"
+          hide-details
+          density="compact"
+          class="ml-2"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="event in events.$items"
+        :key="event.eventId!"
+        cols="12"
+        md="6"
+        lg="4"
+      >
+        <EventCard :event="event" />
+      </v-col>
+    </v-row>
+  </c-loader-status>
 </template>
 
 <script setup lang="ts">
 import { EventListViewModel } from "@/viewmodels.g";
 import { Event } from "@/models.g";
 
+const props = defineProps<{
+  groupId?: number;
+}>();
+
 const { userInfo } = useUser();
 
 const eventsDataSource = new Event.DataSources.EventsByDate();
-eventsDataSource.userId = userInfo.value.id;
 const events = new EventListViewModel();
+
+if (props.groupId) {
+  events.$params.filter = { groupId: props.groupId };
+} else {
+  eventsDataSource.userId = userInfo.value.id;
+}
+
 events.$dataSource = eventsDataSource;
 events.$useAutoLoad();
 events.$load();
