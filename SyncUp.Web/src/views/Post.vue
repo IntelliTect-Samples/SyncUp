90e10a774565a22285ff36314d87bc451a125c5b
Olivia Smith <osmith.vendor@n2y.com>
2024-11-15 20:23:43 -0800

    Post page

---
+++ SyncUp.Web/src/views/Post.vue
@@ -0,0 +1,22 @@
+<template>
+<v-container>
+    <v-card>
+        <div>
+            <h5>testing testing</h5>
+            {{ post.title }}
+        </div>
+    </v-card>
+</v-container>
+</template>
+<script setup lang="ts">
+import { PostViewModel } from "@/viewmodels.g";
+
+useTitle("Post");
+
+const props = defineProps<{
+  postId: number;
+}>();
+
+const post = new PostViewModel();
+post.$load(props.postId);
+</script>
